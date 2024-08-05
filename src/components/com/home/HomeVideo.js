3e4e12cf3e9fdaedbb413900b808b5541d742354
DESKTOP-7GT0DUQ\user <kkmkk01@naver.com>
2024-08-06 01:52:08 +0900

    MainHome에 동영상 추가, images 파일 이동, videos 파일 추가

--- src/components/com/home/HomeVideo.js
+++ src/components/com/home/HomeVideo.js
@@ -1,8 +1,12 @@
 import React from "react";
+import heroVideo from "../../../assets/videos/hero_n2soft.mp4";
 
 const HomeVideo = () => {
   return (
     <div className="video-main">
+      <video autoPlay loop muted>
+        <source src={heroVideo} type="video/mp4" />
+      </video>
       <h1>최상의 서비스를 더하는</h1>
       <h1>금융시스템 솔루션 전문 IT기업</h1>
       <div className="video-stick"></div>
