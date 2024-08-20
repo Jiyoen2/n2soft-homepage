e341e816ea1d055791ef5706b2ffe304b5ffcf13
DESKTOP-7GT0DUQ\user <kkmkk01@naver.com>
2024-08-20 16:50:26 +0900

    하위 메뉴바 수정(샘플)

--- src/components/com/solution/SolutionTop.js
+++ src/components/com/solution/SolutionTop.js
@@ -3,21 +3,20 @@ import React from "react";
 const SolutionTop = ({ setActiveTab, activeTab }) => {
   return (
     <div className="solution-bg">
+      {/* <div style={{ height: "80px" }}></div> */}
       <div className="top-tit">
         <h1>SOLUTION</h1>
-        <div>
-          <div className="top-menu-com">
-            <div
-              className={`top-menu-1 ${
-                activeTab === "solution" ? "active" : ""
-              }`}
-              onClick={() => setActiveTab("solution")}
-            >
-              솔루션
-            </div>
+      </div>
+      <div>
+        <div className="top-menu-com">
+          <div
+            className={`top-menu-1 ${activeTab === "solution" ? "active" : ""}`}
+            onClick={() => setActiveTab("solution")}
+          >
+            솔루션
           </div>
-          <div className="top-stick"></div>
         </div>
+        <div className="top-stick"></div>
       </div>
     </div>
   );
