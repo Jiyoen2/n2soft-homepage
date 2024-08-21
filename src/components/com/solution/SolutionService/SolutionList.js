41b485b78ecc79161b6247f47578b00afaf82222
DESKTOP-7GT0DUQ\user <kkmkk01@naver.com>
2024-08-21 14:49:37 +0900

    125배율 맞춤

--- src/components/com/solution/SolutionService/SolutionList.js
+++ src/components/com/solution/SolutionService/SolutionList.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-const SolutionList = ({ num, title, content, golink }) => {
+const SolutionList = ({ title, content, golink }) => {
   return (
     <div className="solu-text">
-      <div className="solu-text-num">{num}</div>
+      {/* <div className="solu-text-num">{num}</div> */}
       <div className="solu-text-tit">{title}</div>
       <div className="solu-text-cont">{content}</div>
       <div className="solu-text-link">{golink}</div>
